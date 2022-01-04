03b0a15f4681118fb7f6d01b0de24ac5b0faff80
Carlos A Becker <caarlos0@gmail.com>
2022-01-04 10:10:56 -0300

    feat: WithIdleTimeout and WithMaxTimeout
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- options.go
+++ options.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/charmbracelet/keygen"
 	"github.com/gliderlabs/ssh"
@@ -70,4 +71,20 @@ func WithPublicKeyAuth(h ssh.PublicKeyHandler) ssh.Option {
 // WithPasswordAuth returns an ssh.Option that sets the password auth handler.
 func WithPasswordAuth(p ssh.PasswordHandler) ssh.Option {
 	return ssh.PasswordAuth(p)
+}
+
+// WithIdleTimeout returns an ssh.Option that sets the connection's idle timeout.
+func WithIdleTimeout(d time.Duration) ssh.Option {
+	return func(s *ssh.Server) error {
+		s.IdleTimeout = d
+		return nil
+	}
+}
+
+// WithMaxTimeout returns an ssh.Option that sets the connection's absolute timeout,
+func WithMaxTimeout(d time.Duration) ssh.Option {
+	return func(s *ssh.Server) error {
+		s.MaxTimeout = d
+		return nil
+	}
 }
