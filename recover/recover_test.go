beb5f0cf971cb0f6f8e5cc6fa30a5bf24753a289
Ayman Bagabas <ayman.bagabas@gmail.com>
2022-01-28 16:15:48 -0500

    feat: add recover middleware
    recover middleware wraps other middlewares and recover from panics

---
+++ recover/recover_test.go
@@ -0,0 +1,35 @@
+package recover
+
+import (
+	"testing"
+
+	"github.com/charmbracelet/wish/testsession"
+	"github.com/gliderlabs/ssh"
+	gossh "golang.org/x/crypto/ssh"
+)
+
+func TestMiddleware(t *testing.T) {
+	t.Run("recover session", func(t *testing.T) {
+		_, err := setup(t).Output("")
+		requireNoError(t, err)
+	})
+}
+
+func setup(tb testing.TB) *gossh.Session {
+	tb.Helper()
+	return testsession.New(tb, &ssh.Server{
+		Handler: Middleware(func(h ssh.Handler) ssh.Handler {
+			return func(s ssh.Session) {
+				panic("hello")
+			}
+		})(func(s ssh.Session) {}),
+	}, nil)
+}
+
+func requireNoError(t *testing.T, err error) {
+	t.Helper()
+
+	if err != nil {
+		t.Fatalf("expected no error, got %q", err.Error())
+	}
+}
