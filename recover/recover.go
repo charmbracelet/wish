b355490708efb77edde00b7d9888be6f3f1aeb9f
Ayman Bagabas <ayman.bagabas@gmail.com>
2022-01-28 16:15:48 -0500

    feat: add recover middleware
    recover middleware wraps other middlewares and recover from panics

---
+++ recover/recover.go
@@ -0,0 +1,39 @@
+package recover
+
+import (
+	"log"
+	"runtime/debug"
+
+	"github.com/charmbracelet/wish"
+	"github.com/gliderlabs/ssh"
+)
+
+// Middleware is a wish middleware that recovers from panics and log to stderr.
+func Middleware(mw ...wish.Middleware) wish.Middleware {
+	return MiddlewareWithLogger(nil, mw...)
+}
+
+// MiddlewareWithLogger is a wish middleware that recovers from panics and log to
+// the provided logger.
+func MiddlewareWithLogger(logger *log.Logger, mw ...wish.Middleware) wish.Middleware {
+	if logger == nil {
+		logger = log.Default()
+	}
+	h := func(ssh.Session) {}
+	for _, m := range mw {
+		h = m(h)
+	}
+	return func(sh ssh.Handler) ssh.Handler {
+		return func(s ssh.Session) {
+			func() {
+				defer func() {
+					if r := recover(); r != nil {
+						logger.Printf("panic: %v\n%s", r, string(debug.Stack()))
+					}
+				}()
+				h(s)
+			}()
+			sh(s)
+		}
+	}
+}
