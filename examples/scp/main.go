8ebbfa49b824ca5a7699f907cf4052886aa2de9e
Carlos A Becker <caarlos0@gmail.com>
2022-02-14 16:35:20 -0300

    feat: example
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

---
+++ examples/scp/main.go
@@ -0,0 +1,50 @@
+package main
+
+// An example Bubble Tea server. This will put an ssh session into alt screen
+// and continually print up to date terminal information.
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/charmbracelet/wish"
+	"github.com/charmbracelet/wish/scp"
+)
+
+const host = "localhost"
+const port = 23234
+
+func main() {
+	s, err := wish.NewServer(
+		wish.WithAddress(fmt.Sprintf("%s:%d", host, port)),
+		wish.WithHostKeyPath(".ssh/term_info_ed25519"),
+		wish.WithMiddleware(
+			scp.Middleware(scp.NewFileSystemHandler("./examples/scp/testdata")),
+		),
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	log.Printf("Starting SSH server on %s:%d", host, port)
+	go func() {
+		if err = s.ListenAndServe(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	<-done
+	log.Println("Stopping SSH server")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer func() { cancel() }()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatalln(err)
+	}
+}
