f2e7c2ae5359e63f63357323464768911fc28c29
Carlos A Becker <caarlos0@gmail.com>
2022-02-16 22:21:19 -0300

    refactor
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- scp/copy_to_client.go
+++ scp/copy_to_client.go
@@ -7,40 +7,30 @@ import (
 	"github.com/gliderlabs/ssh"
 )
 
-func copyFilesToClient(s ssh.Session, handler CopyToClientHandler, paths []string) error {
-	entries := &RootEntry{}
-	var closers []func() error
-	defer closeAll(closers)
-
-	for _, path := range paths {
-		entry, closer, err := handler.NewFileEntry(s, path)
-		closers = append(closers, closer)
-		if err != nil {
-			return err
-		}
-		entries.Append(entry)
-	}
-	return entries.Write(s)
-}
-
 func copyToClient(s ssh.Session, info Info, handler CopyToClientHandler) error {
-	paths, err := handler.Glob(s, info.Path)
+	matches, err := handler.Glob(s, info.Path)
 	if err != nil {
 		return err
 	}
-	if len(paths) == 0 {
+	if len(matches) == 0 {
 		return fmt.Errorf("no files matching %q", info.Path)
-	}
-
-	if !info.Recursive {
-		return copyFilesToClient(s, handler, paths)
 	}
 
 	rootEntry := &RootEntry{}
 	var closers []func() error
 	defer closeAll(closers)
 
-	for _, match := range paths {
+	for _, match := range matches {
+		if !info.Recursive {
+			entry, closer, err := handler.NewFileEntry(s, match)
+			closers = append(closers, closer)
+			if err != nil {
+				return err
+			}
+			rootEntry.Append(entry)
+			continue
+		}
+
 		if err := handler.WalkDir(s, match, func(path string, d fs.DirEntry, err error) error {
 			if err != nil {
 				return err
@@ -66,6 +56,7 @@ func copyToClient(s ssh.Session, info Info, handler CopyToClientHandler) error {
 			return err
 		}
 	}
+
 	return rootEntry.Write(s)
 }
 
