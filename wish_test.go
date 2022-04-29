0ff781120fb111084362fd62e3b1a3c15ba057a3
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2022-04-29 15:08:35 -0300

    feat: add wish.Fatal and wish.Error (#41)
    * feat: add wish.Fatal and wish.Error
    
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>
    
    * fix: dont need mocks for this, actually
    
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- wish_test.go
+++ wish_test.go
@@ -1,8 +1,15 @@
+// go:genarate mockgen -package mocks -destination mocks/session.go github.com/gliderlabs/ssh Session
 package wish
 
 import (
+	"bytes"
+	"errors"
+	"strings"
 	"testing"
 	"time"
+
+	"github.com/charmbracelet/wish/testsession"
+	"github.com/gliderlabs/ssh"
 )
 
 func TestNewServer(t *testing.T) {
@@ -19,5 +26,39 @@ func TestNewServerWithOptions(t *testing.T) {
 	)
 	if err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestError(t *testing.T) {
+	eerr := errors.New("foo err")
+	sess := testsession.New(t, &ssh.Server{
+		Handler: func(s ssh.Session) {
+			Error(s, eerr)
+		},
+	}, nil)
+	var out bytes.Buffer
+	sess.Stderr = &out
+	if err := sess.Run(""); err != nil {
+		t.Errorf("expected no error, got %s", err)
+	}
+	if s := strings.TrimSpace(out.String()); s != eerr.Error() {
+		t.Errorf("expected %s, got %s", s, eerr)
+	}
+}
+
+func TestFatal(t *testing.T) {
+	err := errors.New("foo err")
+	sess := testsession.New(t, &ssh.Server{
+		Handler: func(s ssh.Session) {
+			Fatal(s, err)
+		},
+	}, nil)
+	var out bytes.Buffer
+	sess.Stderr = &out
+	if err := sess.Run(""); err == nil {
+		t.Error("expected an error, got nil")
+	}
+	if s := strings.TrimSpace(out.String()); s != err.Error() {
+		t.Errorf("expected %s, got %s", s, err)
 	}
 }
