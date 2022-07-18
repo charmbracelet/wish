8c05a1b0dc0a1e537c7f28740860c9a586052613
Ayman Bagabas <ayman.bagabas@gmail.com>
2022-07-18 12:39:16 -0400

    feat: add elapsed middleware
    logs ssh session duration

---
+++ elapsed/elapsed.go
@@ -0,0 +1,29 @@
+package timer
+
+import (
+	"time"
+
+	"github.com/charmbracelet/wish"
+	"github.com/gliderlabs/ssh"
+)
+
+// MiddlewareWithFormat returns a middleware that logs the elapsed time of the
+// session. It accepts a format string to print the elapsed time.
+//
+// This must be called as the last middleware in the chain.
+func MiddlewareWithFormat(format string) wish.Middleware {
+	return func(sh ssh.Handler) ssh.Handler {
+		return func(s ssh.Session) {
+			now := time.Now()
+			sh(s)
+			wish.Printf(s, format, time.Since(now))
+		}
+	}
+}
+
+// Middleware returns a middleware that logs the elapsed time of the session.
+//
+// This must be called as the last middleware in the chain.
+func Middleware() wish.Middleware {
+	return MiddlewareWithFormat("elapsed time: %v\n")
+}
---
+++ elapsed/elapsed_test.go
@@ -0,0 +1,43 @@
+package timer
+
+import (
+	"testing"
+	"time"
+
+	"github.com/charmbracelet/wish/testsession"
+	"github.com/gliderlabs/ssh"
+	gossh "golang.org/x/crypto/ssh"
+)
+
+var (
+	waitDuration = time.Second
+)
+
+func TestMiddleware(t *testing.T) {
+	t.Run("recover session", func(t *testing.T) {
+		b, err := setup(t).Output("")
+		requireNoError(t, err)
+		dur, err := time.ParseDuration(string(b))
+		requireNoError(t, err)
+		if dur < waitDuration {
+			t.Errorf("expected elapsed time to be at least 1s, got %v", dur)
+		}
+	})
+}
+
+func setup(tb testing.TB) *gossh.Session {
+	tb.Helper()
+	return testsession.New(tb, &ssh.Server{
+		Handler: MiddlewareWithFormat("%v")(func(s ssh.Session) {
+			time.Sleep(waitDuration)
+		}),
+	}, nil)
+}
+
+func requireNoError(t *testing.T, err error) {
+	t.Helper()
+
+	if err != nil {
+		t.Fatalf("expected no error, got %q", err.Error())
+	}
+}
