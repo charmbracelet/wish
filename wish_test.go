7e511f1b3b9968ec42c387ba9c442f746b3d1aae
Martin <spam-github@mdosch.de>
2022-11-03 17:58:05 +0100

    docs: fix typos (#83)

--- wish_test.go
+++ wish_test.go
@@ -1,4 +1,4 @@
-// go:genarate mockgen -package mocks -destination mocks/session.go github.com/gliderlabs/ssh Session
+// go:generate mockgen -package mocks -destination mocks/session.go github.com/gliderlabs/ssh Session
 package wish
 
 import (
