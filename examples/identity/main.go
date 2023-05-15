bca88afcf6e230368fcc47956c7ff67104a52434
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-05-15 13:29:21 +0000

    fix(examples): properly shut down if failed to start
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- examples/identity/main.go
+++ examples/identity/main.go
@@ -55,6 +55,7 @@ func main() {
 	go func() {
 		if err = s.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
 			log.Error("could not start server", "error", err)
+			done <- nil
 		}
 	}()
 
