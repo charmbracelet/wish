2851336e9815d019a9a1337b4237548666bf5685
Maas Lalani <maas@lalani.dev>
2023-06-01 12:04:25 -0400

    docs(examples): add ssh chat example

---
+++ examples/multichat/main.go
@@ -0,0 +1,197 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/ssh"
+	"github.com/charmbracelet/wish"
+	bm "github.com/charmbracelet/wish/bubbletea"
+	lm "github.com/charmbracelet/wish/logging"
+	"github.com/muesli/termenv"
+)
+
+const (
+	host = "localhost"
+	port = 23234
+)
+
+// app contains a wish server and the list of running programs.
+type app struct {
+	*ssh.Server
+	progs []*tea.Program
+}
+
+// send dispatches a message to all running programs.
+func (a *app) send(msg tea.Msg) {
+	for _, p := range a.progs {
+		go p.Send(msg)
+	}
+}
+
+func newApp() *app {
+	a := new(app)
+
+	s, err := wish.NewServer(
+		wish.WithAddress(fmt.Sprintf("%s:%d", host, port)),
+		wish.WithHostKeyPath(".ssh/term_info_ed25519"),
+		wish.WithMiddleware(
+			bm.MiddlewareWithProgramHandler(a.ProgramHandler, termenv.ANSI256),
+			lm.Middleware(),
+		),
+	)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	a.Server = s
+	return a
+}
+
+func (a *app) Start() {
+	var err error
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	log.Printf("Starting SSH server on %s:%d", host, port)
+	go func() {
+		if err = a.ListenAndServe(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	<-done
+	log.Println("Stopping SSH server")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer func() { cancel() }()
+	if err := a.Shutdown(ctx); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func (a *app) ProgramHandler(s ssh.Session) *tea.Program {
+	if _, _, active := s.Pty(); !active {
+		wish.Fatalln(s, "terminal is not active")
+	}
+
+	model := initialModel()
+	model.app = a
+	model.id = s.User()
+
+	p := tea.NewProgram(model, tea.WithOutput(s), tea.WithInput(s))
+	a.progs = append(a.progs, p)
+
+	return p
+}
+
+func main() {
+	app := newApp()
+	app.Start()
+}
+
+type (
+	errMsg  error
+	chatMsg struct {
+		id   string
+		text string
+	}
+)
+
+type model struct {
+	*app
+	viewport    viewport.Model
+	messages    []string
+	id          string
+	textarea    textarea.Model
+	senderStyle lipgloss.Style
+	err         error
+}
+
+func initialModel() model {
+	ta := textarea.New()
+	ta.Placeholder = "Send a message..."
+	ta.Focus()
+
+	ta.Prompt = "┃ "
+	ta.CharLimit = 280
+
+	ta.SetWidth(30)
+	ta.SetHeight(3)
+
+	// Remove cursor line styling
+	ta.FocusedStyle.CursorLine = lipgloss.NewStyle()
+
+	ta.ShowLineNumbers = false
+
+	vp := viewport.New(30, 5)
+	vp.SetContent(`Welcome to the chat room!
+Type a message and press Enter to send.`)
+
+	ta.KeyMap.InsertNewline.SetEnabled(false)
+
+	return model{
+		textarea:    ta,
+		messages:    []string{},
+		viewport:    vp,
+		senderStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("5")),
+		err:         nil,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textarea.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		tiCmd tea.Cmd
+		vpCmd tea.Cmd
+	)
+
+	m.textarea, tiCmd = m.textarea.Update(msg)
+	m.viewport, vpCmd = m.viewport.Update(msg)
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		case tea.KeyEnter:
+			m.app.send(chatMsg{
+				id:   m.id,
+				text: m.textarea.Value(),
+			})
+			m.textarea.Reset()
+		}
+
+	case chatMsg:
+		m.messages = append(m.messages, m.senderStyle.Render(msg.id)+": "+msg.text)
+		m.viewport.SetContent(strings.Join(m.messages, "\n"))
+		m.viewport.GotoBottom()
+
+	// We handle errors just like any other message
+	case errMsg:
+		m.err = msg
+		return m, nil
+	}
+
+	return m, tea.Batch(tiCmd, vpCmd)
+}
+
+func (m model) View() string {
+	return fmt.Sprintf(
+		"%s\n\n%s",
+		m.viewport.View(),
+		m.textarea.View(),
+	) + "\n\n"
+}
