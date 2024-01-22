0b7f07cf36c2a087423c7c443c4416ee49423f8b
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-01-22 14:47:02 -0300

    test: windows
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- cmd_test.go
+++ cmd_test.go
@@ -2,6 +2,7 @@ package wish
 
 import (
 	"bytes"
+	"runtime"
 	"strings"
 	"testing"
 
@@ -13,28 +14,9 @@ func TestCommandNoPty(t *testing.T) {
 	tmp := t.TempDir()
 	sess := testsession.New(t, &ssh.Server{
 		Handler: func(s ssh.Session) {
-			if err := Command(s, "echo", "hello").Run(); err != nil {
-				Fatal(s, err)
-			}
-
-			cmd := Command(s, "env")
-			cmd.SetEnv([]string{"HELLO=hello"})
-			if len(cmd.Environ()) != 1 {
-				Fatal(s, "unexpected cmd environ:", cmd.Environ())
-			}
-			if err := cmd.Run(); err != nil {
-				Fatal(s, err)
-			}
-
-			cmd = Command(s, "pwd")
-			cmd.SetDir(tmp)
-			// these should do nothing...
-			cmd.SetStderr(nil)
-			cmd.SetStdin(nil)
-			cmd.SetStdout(nil)
-			if err := cmd.Run(); err != nil {
-				Fatal(s, err)
-			}
+			runEcho(s, "hello")
+			runEnv(s, []string{"HELLO=world"})
+			runPwd(s, tmp)
 		},
 	}, nil)
 	var stdout bytes.Buffer
@@ -44,39 +26,19 @@ func TestCommandNoPty(t *testing.T) {
 	if err := sess.Run(""); err != nil {
 		t.Errorf("expected no error, got %v: %s", err, stderr.String())
 	}
-	expect := strings.Join([]string{
-		"hello",
-		"HELLO=hello",
-		tmp,
-	}, "\n") + "\n"
-	if s := stdout.String(); s != expect {
-		t.Errorf("expected output to be %q, got %q", expect, s)
-	}
+	out := stdout.String()
+	expectContains(t, out, "hello")
+	expectContains(t, out, "HELLO=world")
+	expectContains(t, out, tmp)
 }
 
 func TestCommandPty(t *testing.T) {
 	tmp := t.TempDir()
 	srv := &ssh.Server{
 		Handler: func(s ssh.Session) {
-			if err := Command(s, "echo", "hello").Run(); err != nil {
-				Fatal(s, err)
-			}
-
-			cmd := Command(s, "env")
-			cmd.SetEnv([]string{"HELLO=hello"})
-			if err := cmd.Run(); err != nil {
-				Fatal(s, err)
-			}
-
-			cmd = Command(s, "pwd")
-			cmd.SetDir(tmp)
-			// these should do nothing...
-			cmd.SetStderr(nil)
-			cmd.SetStdin(nil)
-			cmd.SetStdout(nil)
-			if err := cmd.Run(); err != nil {
-				Fatal(s, err)
-			}
+			runEcho(s, "hello")
+			runEnv(s, []string{"HELLO=world"})
+			runPwd(s, tmp)
 		},
 	}
 	if err := ssh.AllocatePty()(srv); err != nil {
@@ -95,14 +57,10 @@ func TestCommandPty(t *testing.T) {
 	if err := sess.Run(""); err != nil {
 		t.Errorf("expected no error, got %v: %s", err, stderr.String())
 	}
-	expect := strings.Join([]string{
-		"hello",
-		"HELLO=hello",
-		tmp,
-	}, "\r\n") + "\r\n"
-	if s := stdout.String(); s != expect {
-		t.Errorf("expected output to be %q, got %q", expect, s)
-	}
+	out := stdout.String()
+	expectContains(t, out, "hello")
+	expectContains(t, out, "HELLO=world")
+	expectContains(t, out, tmp)
 }
 
 func TestCommandPtyError(t *testing.T) {
@@ -130,5 +88,50 @@ func TestCommandPtyError(t *testing.T) {
 	expect := `exec: "nopenopenope"`
 	if s := stderr.String(); !strings.Contains(s, expect) {
 		t.Errorf("expected output to contain %q, got %q", expect, s)
+	}
+}
+
+func runEcho(s ssh.Session, str string) {
+	cmd := Command(s, "echo", str)
+	if runtime.GOOS == "windows" {
+		cmd = Command(s, "cmd", "/C", "echo", str)
+	}
+	// these should do nothing...
+	cmd.SetStderr(nil)
+	cmd.SetStdin(nil)
+	cmd.SetStdout(nil)
+	if err := cmd.Run(); err != nil {
+		Fatal(s, err)
+	}
+}
+
+func runEnv(s ssh.Session, env []string) {
+	cmd := Command(s, "env")
+	if runtime.GOOS == "windows" {
+		cmd = Command(s, "cmd", "/C", "set")
+	}
+	cmd.SetEnv(env)
+	if err := cmd.Run(); err != nil {
+		Fatal(s, err)
+	}
+	if len(cmd.Environ()) == 0 {
+		Fatal(s, "cmd.Environ() should not be empty")
+	}
+}
+
+func runPwd(s ssh.Session, dir string) {
+	cmd := Command(s, "pwd")
+	if runtime.GOOS == "windows" {
+		cmd = Command(s, "cmd", "/C", "cd")
+	}
+	cmd.SetDir(dir)
+	if err := cmd.Run(); err != nil {
+		Fatal(s, err)
+	}
+}
+
+func expectContains(tb testing.TB, s, substr string) {
+	if !strings.Contains(s, substr) {
+		tb.Errorf("expected output %q to contain %q", s, substr)
 	}
 }
