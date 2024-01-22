32aeafcecb9d2832f7a48258ed4c3ee18cecec9a
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-01-22 14:55:22 -0300

    fix: review
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- cmd.go
+++ cmd.go
@@ -1,21 +1,23 @@
 package wish
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"os/exec"
 	"runtime"
 	"time"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/ssh"
 )
 
-// Command sets stdin, stdout, and stderr to the current session's PTY slave.
+// CommandContext is like Command but includes a context.
 //
 // If the current session does not have a PTY, it sets them to the session
 // itself.
-func Command(s ssh.Session, name string, args ...string) *Cmd {
-	c := exec.Command(name, args...)
+func CommandContext(ctx context.Context, s ssh.Session, name string, args ...string) *Cmd {
+	c := exec.CommandContext(ctx, name, args...)
 	pty, _, ok := s.Pty()
 	if !ok {
 		c.Stdin, c.Stdout, c.Stderr = s, s, s
@@ -23,6 +25,16 @@ func Command(s ssh.Session, name string, args ...string) *Cmd {
 	}
 
 	return &Cmd{c, &pty}
+}
+
+// Command sets stdin, stdout, and stderr to the current session's PTY slave.
+//
+// If the current session does not have a PTY, it sets them to the session
+// itself.
+//
+// This will call CommandContext using the session's Context.
+func Command(s ssh.Session, name string, args ...string) *Cmd {
+	return CommandContext(s.Context(), s, name, args...)
 }
 
 // Cmd wraps a *exec.Cmd and a ssh.Pty so a command can be properly run.
@@ -45,15 +57,6 @@ func (c *Cmd) Environ() []string {
 func (c *Cmd) SetDir(dir string) {
 	c.cmd.Dir = dir
 }
-
-// SetStderr conforms with tea.ExecCommand.
-func (*Cmd) SetStderr(io.Writer) {}
-
-// SetStdin conforms with tea.ExecCommand.
-func (*Cmd) SetStdin(io.Reader) {}
-
-// SetStdout conforms with tea.ExecCommand.
-func (*Cmd) SetStdout(io.Writer) {}
 
 // Run runs the program and waits for it to finish.
 func (c *Cmd) Run() error {
@@ -83,3 +86,14 @@ func (c *Cmd) Run() error {
 	}
 	return nil
 }
+
+var _ tea.ExecCommand = &Cmd{}
+
+// SetStderr conforms with tea.ExecCommand.
+func (*Cmd) SetStderr(io.Writer) {}
+
+// SetStdin conforms with tea.ExecCommand.
+func (*Cmd) SetStdin(io.Reader) {}
+
+// SetStdout conforms with tea.ExecCommand.
+func (*Cmd) SetStdout(io.Writer) {}
