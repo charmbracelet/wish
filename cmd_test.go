7491d08d6e4cdbab128d17763886184af73ae900
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-01-22 13:20:30 -0300

    test: more tests
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- cmd_test.go
+++ cmd_test.go
@@ -14,7 +14,7 @@ func TestCommandNoPty(t *testing.T) {
 	sess := testsession.New(t, &ssh.Server{
 		Handler: func(s ssh.Session) {
 			if err := Command(s, "echo", "hello").Run(); err != nil {
-				Fatal(s, "echo:", err)
+				Fatal(s, err)
 			}
 
 			cmd := Command(s, "env")
@@ -23,13 +23,17 @@ func TestCommandNoPty(t *testing.T) {
 				Fatal(s, "unexpected cmd environ:", cmd.Environ())
 			}
 			if err := cmd.Run(); err != nil {
-				Fatal(s, "env:", err)
+				Fatal(s, err)
 			}
 
 			cmd = Command(s, "pwd")
 			cmd.SetDir(tmp)
+			// these should do nothing...
+			cmd.SetStderr(nil)
+			cmd.SetStdin(nil)
+			cmd.SetStdout(nil)
 			if err := cmd.Run(); err != nil {
-				Fatal(s, "pwd:", err)
+				Fatal(s, err)
 			}
 		},
 	}, nil)
@@ -53,19 +57,23 @@ func TestCommandPty(t *testing.T) {
 	srv := &ssh.Server{
 		Handler: func(s ssh.Session) {
 			if err := Command(s, "echo", "hello").Run(); err != nil {
-				Fatal(s, "echo:", err)
+				Fatal(s, err)
 			}
 
 			cmd := Command(s, "env")
 			cmd.SetEnv([]string{"HELLO=hello"})
 			if err := cmd.Run(); err != nil {
-				Fatal(s, "env:", err)
+				Fatal(s, err)
 			}
 
 			cmd = Command(s, "pwd")
 			cmd.SetDir(tmp)
+			// these should do nothing...
+			cmd.SetStderr(nil)
+			cmd.SetStdin(nil)
+			cmd.SetStdout(nil)
 			if err := cmd.Run(); err != nil {
-				Fatal(s, "pwd:", err)
+				Fatal(s, err)
 			}
 		},
 	}
@@ -90,5 +98,33 @@ func TestCommandPty(t *testing.T) {
 	}, "\r\n") + "\r\n"
 	if s := out.String(); s != expect {
 		t.Errorf("expected output to be %q, got %q", expect, s)
+	}
+}
+
+func TestCommandPtyError(t *testing.T) {
+	srv := &ssh.Server{
+		Handler: func(s ssh.Session) {
+			if err := Command(s, "nopenopenope").Run(); err != nil {
+				Fatal(s, err)
+			}
+		},
+	}
+	if err := ssh.AllocatePty()(srv); err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+
+	sess := testsession.New(t, srv, nil)
+	if err := sess.RequestPty("xterm", 500, 200, nil); err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+
+	var out bytes.Buffer
+	sess.Stderr = &out
+	if err := sess.Run(""); err == nil {
+		t.Errorf("expected an error, got nil")
+	}
+	expect := `exec: "nopenopenope"`
+	if s := out.String(); !strings.Contains(s, expect) {
+		t.Errorf("expected output to contain %q, got %q", expect, s)
 	}
 }
