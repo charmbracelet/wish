692d004c4bb9a5213a92921a9c2bf4e6b25a6cee
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-01-24 12:26:39 -0300

    fix: windows/linux
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- cmd_unix.go
+++ cmd_unix.go
@@ -5,7 +5,7 @@ package wish
 
 import "github.com/charmbracelet/ssh"
 
-func (c *Cmd) doRun(ppty ssh.Pty) error {
+func (c *Cmd) doRun(ppty ssh.Pty, _ <-chan ssh.Window) error {
 	if err := ppty.Start(c.cmd); err != nil {
 		return err
 	}
--- cmd_windows.go
+++ cmd_windows.go
@@ -10,7 +10,7 @@ import (
 	"github.com/charmbracelet/ssh"
 )
 
-func (c *Cmd) doRun(ppty ssh.Pty) error {
+func (c *Cmd) doRun(ppty ssh.Pty, _ <-chan ssh.Window) error {
 	if err := ppty.Start(c.cmd); err != nil {
 		return err
 	}
