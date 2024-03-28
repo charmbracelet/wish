0169ff6c94693eb96c9a3ddc07852790ab333bbd
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-03-28 14:12:08 -0300

    feat: logging.StructuredMiddleware (#254)
    Supports logging with charmbracelet/log in a structured format,
    especially useful with JSON logs.

--- logging/logging.go
+++ logging/logging.go
@@ -8,12 +8,12 @@ import (
 	"github.com/charmbracelet/wish"
 )
 
-// Middleware provides basic connection logging. Connects are logged with the
-// remote address, invoked command, TERM setting, window dimensions and if the
-// auth was public key based. Disconnect will log the remote address and
-// connection duration.
+// Middleware provides basic connection logging.
+// Connects are logged with the remote address, invoked command, TERM setting,
+// window dimensions, client version, and if the auth was public key based.
+// Disconnect will log the remote address and connection duration.
 //
-// The logger is set to the std default logger.
+// It will use charmbracelet/log.StandardLog() by default.
 func Middleware() wish.Middleware {
 	return MiddlewareWithLogger(log.StandardLog())
 }
@@ -23,10 +23,10 @@ type Logger interface {
 	Printf(format string, v ...interface{})
 }
 
-// MiddlewareWithLogger provides basic connection logging. Connects are logged with the
-// remote address, invoked command, TERM setting, window dimensions and if the
-// auth was public key based. Disconnect will log the remote address and
-// connection duration.
+// MiddlewareWithLogger provides basic connection logging.
+// Connects are logged with the remote address, invoked command, TERM setting,
+// window dimensions, client version, and if the auth was public key based.
+// Disconnect will log the remote address and connection duration.
 func MiddlewareWithLogger(logger Logger) wish.Middleware {
 	return func(next ssh.Handler) ssh.Handler {
 		return func(sess ssh.Session) {
@@ -34,7 +34,7 @@ func MiddlewareWithLogger(logger Logger) wish.Middleware {
 			hpk := sess.PublicKey() != nil
 			pty, _, _ := sess.Pty()
 			logger.Printf(
-				"%s connect %s %v %v %s %v %v",
+				"%s connect %s %v %v %s %v %v %v",
 				sess.User(),
 				sess.RemoteAddr().String(),
 				hpk,
@@ -42,12 +42,57 @@ func MiddlewareWithLogger(logger Logger) wish.Middleware {
 				pty.Term,
 				pty.Window.Width,
 				pty.Window.Height,
+				sess.Context().ClientVersion(),
 			)
 			next(sess)
 			logger.Printf(
 				"%s disconnect %s\n",
 				sess.RemoteAddr().String(),
 				time.Since(ct),
+			)
+		}
+	}
+}
+
+// StructuredMiddleware provides basic connection logging in a structured form.
+// Connects are logged with the remote address, invoked command, TERM setting,
+// window dimensions, client version, and if the auth was public key based.
+// Disconnect will log the remote address and connection duration.
+//
+// It will use the charmbracelet/log.Default() and Info level by default.
+func StructuredMiddleware() wish.Middleware {
+	return StructuredMiddlewareWithLogger(log.Default(), log.InfoLevel)
+}
+
+// StructuredMiddlewareWithLogger provides basic connection logging in a structured form.
+// Connects are logged with the remote address, invoked command, TERM setting,
+// window dimensions, client version, and if the auth was public key based.
+// Disconnect will log the remote address and connection duration.
+func StructuredMiddlewareWithLogger(logger *log.Logger, level log.Level) wish.Middleware {
+	return func(next ssh.Handler) ssh.Handler {
+		return func(sess ssh.Session) {
+			ct := time.Now()
+			hpk := sess.PublicKey() != nil
+			pty, _, _ := sess.Pty()
+			logger.Log(
+				level,
+				"connect",
+				"user", sess.User(),
+				"remote-addr", sess.RemoteAddr().String(),
+				"public-key", hpk,
+				"command", sess.Command(),
+				"term", pty.Term,
+				"width", pty.Window.Width,
+				"height", pty.Window.Height,
+				"client-version", sess.Context().ClientVersion(),
+			)
+			next(sess)
+			logger.Log(
+				level,
+				"disconnect",
+				"user", sess.User(),
+				"remote-addr", sess.RemoteAddr().String(),
+				"duration", time.Since(ct),
 			)
 		}
 	}
--- logging/logging_test.go
+++ logging/logging_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/charmbracelet/ssh"
+	"github.com/charmbracelet/wish"
 	"github.com/charmbracelet/wish/logging"
 	"github.com/charmbracelet/wish/testsession"
 	gossh "golang.org/x/crypto/ssh"
@@ -11,16 +12,24 @@ import (
 
 func TestMiddleware(t *testing.T) {
 	t.Run("inactive term", func(t *testing.T) {
-		if err := setup(t).Run(""); err != nil {
+		if err := setup(t, logging.Middleware()).Run(""); err != nil {
 			t.Error(err)
 		}
 	})
 }
 
-func setup(tb testing.TB) *gossh.Session {
+func TestStructuredMiddleware(t *testing.T) {
+	t.Run("inactive term", func(t *testing.T) {
+		if err := setup(t, logging.StructuredMiddleware()).Run(""); err != nil {
+			t.Error(err)
+		}
+	})
+}
+
+func setup(tb testing.TB, middleware wish.Middleware) *gossh.Session {
 	tb.Helper()
 	return testsession.New(tb, &ssh.Server{
-		Handler: logging.Middleware()(func(s ssh.Session) {
+		Handler: middleware(func(s ssh.Session) {
 			s.Write([]byte("hello"))
 		}),
 	}, nil)
