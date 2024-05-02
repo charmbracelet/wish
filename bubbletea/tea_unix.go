a58be5cf1f7ba71b25c1c467a02dce771347ea2a
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-05-02 15:35:38 -0300

    feat: background color detection (#266)
    use our new term lib to query the bg, set it into the lipgloss renderer
    so it works with everything.
    
    refs #262

--- bubbletea/tea_unix.go
+++ bubbletea/tea_unix.go
@@ -7,6 +7,8 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/ssh"
+	"github.com/charmbracelet/x/exp/term"
+	"github.com/lucasb-eyer/go-colorful"
 	"github.com/muesli/termenv"
 )
 
@@ -29,7 +31,23 @@ func newRenderer(s ssh.Session) *lipgloss.Renderer {
 	pty, _, ok := s.Pty()
 	env := sshEnviron(append(s.Environ(), "TERM="+pty.Term))
 	if !ok || pty.Slave == nil {
-		return lipgloss.NewRenderer(s, termenv.WithEnvironment(env), termenv.WithUnsafe(), termenv.WithColorCache(true))
+		return lipgloss.NewRenderer(
+			s,
+			termenv.WithEnvironment(env),
+			termenv.WithUnsafe(),
+			termenv.WithColorCache(true),
+		)
 	}
-	return lipgloss.NewRenderer(pty.Slave, termenv.WithEnvironment(env), termenv.WithColorCache(true))
+	bg := term.BackgroundColor(pty.Slave, pty.Slave)
+	r := lipgloss.NewRenderer(
+		pty.Slave,
+		termenv.WithEnvironment(env),
+		termenv.WithColorCache(true),
+	)
+	c, ok := colorful.MakeColor(bg)
+	if ok {
+		_, _, l := c.Hsl()
+		r.SetHasDarkBackground(l < 0.5)
+	}
+	return r
 }
