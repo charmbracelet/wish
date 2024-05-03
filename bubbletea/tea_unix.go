6e99b230069ef688c163a954c1d444708e9f3178
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-05-03 11:47:55 -0300

    fix: detect bg when no allocate pty
    refs #262

--- bubbletea/tea_unix.go
+++ bubbletea/tea_unix.go
@@ -4,10 +4,14 @@
 package bubbletea
 
 import (
+	"image/color"
+
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/ssh"
 	"github.com/charmbracelet/x/exp/term"
+	"github.com/charmbracelet/x/exp/term/ansi"
+	"github.com/charmbracelet/x/exp/term/input"
 	"github.com/lucasb-eyer/go-colorful"
 	"github.com/muesli/termenv"
 )
@@ -30,24 +34,45 @@ func makeOpts(s ssh.Session) []tea.ProgramOption {
 func newRenderer(s ssh.Session) *lipgloss.Renderer {
 	pty, _, ok := s.Pty()
 	env := sshEnviron(append(s.Environ(), "TERM="+pty.Term))
-	if !ok || pty.Slave == nil {
-		return lipgloss.NewRenderer(
+	var r *lipgloss.Renderer
+	var bg color.Color
+	if ok && pty.Slave != nil {
+		r = lipgloss.NewRenderer(
+			pty.Slave,
+			termenv.WithEnvironment(env),
+			termenv.WithColorCache(true),
+		)
+		bg = term.BackgroundColor(pty.Slave, pty.Slave)
+	} else {
+		r = lipgloss.NewRenderer(
 			s,
 			termenv.WithEnvironment(env),
 			termenv.WithUnsafe(),
 			termenv.WithColorCache(true),
 		)
+		bg = queryBackgroundColor(s)
 	}
-	bg := term.BackgroundColor(pty.Slave, pty.Slave)
-	r := lipgloss.NewRenderer(
-		pty.Slave,
-		termenv.WithEnvironment(env),
-		termenv.WithColorCache(true),
-	)
 	c, ok := colorful.MakeColor(bg)
 	if ok {
 		_, _, l := c.Hsl()
 		r.SetHasDarkBackground(l < 0.5)
 	}
 	return r
+}
+
+// copied from x/exp/term
+func queryBackgroundColor(s ssh.Session) (bg color.Color) {
+	_ = term.QueryTerminal(s, s, func(events []input.Event) bool {
+		for _, e := range events {
+			switch e := e.(type) {
+			case input.BackgroundColorEvent:
+				bg = e.Color
+				continue // we need to consume the next DA1 event
+			case input.PrimaryDeviceAttributesEvent:
+				return false
+			}
+		}
+		return true
+	}, ansi.RequestBackgroundColor+ansi.RequestPrimaryDeviceAttributes)
+	return
 }
